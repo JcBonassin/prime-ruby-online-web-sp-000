caf99541148d116a47971928295cab1139a03926
JC <bonassin@gmail.com>
2020-12-22 19:48:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,6 @@
-def is_prime?(number)
-       return false if number <2
-        (2..Integer.sqrt(number)).each do |divisor|
-              return false if number % divisor == 0
-  end
-     return number
-end
-
-def read_array(array)
-     primes = []
-  array.each do |num|
-       primes << num if is_prime?(num)
-  end
- return primes
+ef prime?(number)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+  (2..number-1).none? {|n|number % n == 0}
 end
