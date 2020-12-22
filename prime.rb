405e5505d93edb18eecba0e01b3f60a8cf302537
JC <bonassin@gmail.com>
2020-12-22 20:03:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,11 @@ def prime?(value)
   if value <= 1 || value == 0 || value == 1
     return false
   elsif
-    (2..value - 1).none? {|i|value % i == 0} end
+    (2..value - 1).none? {|i|value % i == 0}
+      if value % i == 0
+        return false
+      end
+    end
   end
+  true  # <= need explanation
 end
